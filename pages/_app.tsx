eb551783dab8630aa6308666d9a91d6f392909c9
Yuri Toledo <yuriwtoledo@gmail.com>
2022-05-24 18:46:33 -0300

    chore: adding react-query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import type { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider resetCSS>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
