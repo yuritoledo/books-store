54360fb9d61b0053425ff378cb51687053fa6af6
Yuri Toledo <yuriwtoledo@gmail.com>
2022-05-24 18:47:23 -0300

    feat: add simple book list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next"
+import { useQuery } from "react-query"
+import NewBookModal from "../components/NewBook"
+import { getBooks } from "../service/books"
 
 const Home: NextPage = () => {
-  return <></>
+  const query = useQuery("books", getBooks)
+
+  return (
+    <>
+      {query.data?.map((x) => (
+        <div key={x.id}>
+          <h1>{x.title}</h1>
+          <p>{x.author}</p>
+          <p>{x.description}</p>
+          <img src={x.imageUrl} alt="book" />
+        </div>
+      ))}
+
+      <NewBookModal />
+    </>
+  )
 }
 
 export default Home
